dd8e8dc93b5cc713e41247b2ee76731494c64834
Michael Angelo E. Cequina <macequina@gmail.com>
2016-08-05 09:38:09 +0800

    Configure Capistrano for Stage Deployment

--- site/config/deploy.rb
+++ site/config/deploy.rb
@@ -1,17 +1,17 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+lock '3.4.1'
 
 set :stages, ["staging", "production"]
-set :default_stage, "production"
+set :default_stage, "staging"
 
-set :application, 'tonermart'
-set :repo_url, 'git@github.com:tinbot-development/tonermart.git'
+set :application, 'smartamusements'
+set :repo_url, 'git@github.com:tinbot-development/smartamusements.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/var/www/tonermart.tinbotdevelopment.com/htdocs'
+set :deploy_to, '/var/www/smartamusements.tinbotdevelopment.com'
 set :keep_releases, 1
 
 namespace :deploy do
